9925fe6d7c28d77eff3ae96331c9b37e5fc1625f
SSSpac <saifeisa820@gmail.com>
2024-12-28 18:03:47 +0100

    added a cancel button
    now the user gets a new message when they re use the same letter twice, and the user can now leave the game.

--- mains.js
+++ mains.js
@@ -8,37 +8,59 @@ let randomCategory = allKeys[Math.floor(Math.random() * allKeys.length)];
 let randomWord = words[randomCategory][Math.floor(Math.random() * words[randomCategory].length)].toLowerCase();
 
 document.querySelector(".game-info .category span").innerHTML = randomCategory;
-
 let guessedWordArray = randomWord.split("").map(letter => (letter === " " ? " " : "_"));
 let wrongAttempts = 0;
 const maxWrongAttempts = 8;
+let guessedLetters = new Set();
+let gameActive = true; 
 
 function displayGuessedWord() {
     let display = guessedWordArray.map(letter => (letter === " " ? " " : letter)).join(" ");
     document.querySelector(".letters-guess").innerHTML = display;
 }
 
+function quitGame() {
+    gameActive = false;
+    alert("You quit the game. Thanks for playing!");
+}
+
 displayGuessedWord();
 
-while (wrongAttempts < maxWrongAttempts && guessedWordArray.includes("_")) {
+let cancelButton = document.createElement("button");
+cancelButton.innerText = "Cancel Game";
+cancelButton.addEventListener("click", quitGame);
+document.body.appendChild(cancelButton);
+
+while (gameActive && wrongAttempts < maxWrongAttempts && guessedWordArray.includes("_")) {
     let guess = prompt(`Guess a letter (Category: ${randomCategory}):\n${guessedWordArray.join(" ")}`);
-    
+
+    if (guess === null) {
+        quitGame();
+        break;
+    }
+
     if (!guess || guess.length !== 1 || !/^[a-z]$/i.test(guess)) {
-        alert("Please enter a valid single letter.");
+        alert("Please you mut enter a valid  letter.");
         continue;
     }
 
     guess = guess.toLowerCase();
+
+    if (guessedLetters.has(guess)) {
+        alert(`You've already guesed "${guess}". Maybe Try a new letters`);
+        continue;
+    }
+
+    guessedLetters.add(guess);
+
     if (randomWord.includes(guess)) {
-       
         randomWord.split("").forEach((letter, index) => {
             if (letter === guess) {
                 guessedWordArray[index] = guess;
             }
         });
-        alert("Correct guess!");
+        alert(" that is Correct guess!");
     } else {
-        
         wrongAttempts++;
         alert(`Wrong guess! Attempts left: ${maxWrongAttempts - wrongAttempts}`);
         document.querySelector(".hangman-draw").classList.add(`wrong-${wrongAttempts}`);
@@ -46,16 +68,17 @@ while (wrongAttempts < maxWrongAttempts && guessedWordArray.includes("_")) {
 
     displayGuessedWord();
 }
+if (gameActive) {
+    if (guessedWordArray.includes("_")) {
+        alert(`Game Over :( The word was: ${randomWord}`);
+    } else {
+        alert(`Congratolations! You guessed the word: ${randomWord}`);
+    }
 
-if (guessedWordArray.includes("_")) {
-    alert(`Game Over :(  The word was: ${randomWord}`);
-} else {
-    alert(`Congrato lations! You guessed the word: ${randomWord}`);
+    let popup = document.createElement("div");
+    popup.className = "popup";
+    popup.innerText = guessedWordArray.includes("_")
+        ? `Game Over :( The word was: ${randomWord}`
+        : `Congratolations! You guessed the word: ${randomWord}`;
+    document.body.appendChild(popup);
 }
-
-let popup = document.createElement("div");
-popup.className = "popup";
-popup.innerText = guessedWordArray.includes("_")
-    ? `Game Over:(  The word was: ${randomWord}`
-    : `Congratolations! You guessed the word: ${randomWord}`;
-document.body.appendChild(popup);
